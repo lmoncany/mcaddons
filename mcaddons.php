4ceca49e7b7f0b675e76a33ba17103f29684e9b9
lmoncany <l.moncany@gmail.com>
2021-10-21 19:30:16 +0200

    dss

--- mcaddons.php
+++ mcaddons.php
@@ -217,16 +217,13 @@ function display_client_reviews($atts) {
 
 $repeat_field = get_post_meta( get_the_ID(), 'client_reviews_copy');
 ob_start();
-var_dump($repeat_field);
-  if ( $repeat_field != null ) {
+  var_dump($repeat_field);
+  if ( $repeat_field  != null) {
   foreach ($repeat_field as $field) {
   $values = explode( '| ', $field );
-  var_dump($field);
   echo "<div class='review'><div class='testimonial'>$values[0]</div><span class='name' style='margin-top: 10px;'>⭐⭐⭐⭐⭐ - $values[1]</span></div>";
   }
-} else {
-  echo '<p id="noreviews">no reviews yet</p>';
-}
+  }
 
 return ob_get_clean();
 }
